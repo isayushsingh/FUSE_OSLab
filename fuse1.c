b2c0dc0d7b143971dea0bf39555e0575eb4a5d72
Ayush Singh <isayushsingh@gmail.com>
2018-09-25 15:27:49 +0530

    Add files via upload

---
+++ fuse1.c
@@ -0,0 +1,115 @@
+#define FUSE_USE_VERSION 26
+
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+static char hello_str[] = "Everybody lies. The only variable is about what!\n";
+//static char *hello_str = (char*)malloc(strlen(def_str)); // = "Hello World!\n";
+static const char *hello_path = "/virus";
+
+static int hello_getattr(const char *path, struct stat *stbuf)
+{
+	int res = 0;
+	memset(stbuf, 0, sizeof(struct stat));
+	stbuf->st_uid = getuid();
+	stbuf->st_gid = getgid();
+	stbuf->st_atime = stbuf->st_mtime = time(NULL);
+	if (strcmp(path, "/") == 0) {
+		stbuf->st_mode = S_IFDIR | 0755;
+		stbuf->st_nlink = 2;
+	} else if (strcmp(path, hello_path) == 0) {
+		stbuf->st_mode = S_IFREG | 0777;
+		stbuf->st_nlink = 1;
+		stbuf->st_size = strlen(hello_str);
+	} else
+		res = -ENOENT;
+
+	return res;
+}
+
+static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+			 off_t offset, struct fuse_file_info *fi)
+{
+	(void) offset;
+	(void) fi;
+
+	if (strcmp(path, "/") != 0)
+		return -ENOENT;
+
+	filler(buf, ".", NULL, 0);
+	filler(buf, "..", NULL, 0);
+	filler(buf, hello_path+1, NULL, 0);
+
+	return 0;
+}
+
+static int hello_open(const char *path, struct fuse_file_info *fi)
+{
+	if (strcmp(path, hello_path) != 0)
+		return -ENOENT;
+
+	return 0;
+}
+
+static int hello_read(const char *path, char *buf, size_t size, off_t offset,
+		      struct fuse_file_info *fi)
+{
+	size_t len;
+	(void) fi;
+
+	if(strcmp(path, hello_path) != 0)
+		return -ENOENT;
+
+	len = strlen(hello_str);
+	if (offset < len) {
+		if (offset + size > len)
+			size = len - offset;
+		memcpy(buf, hello_str + offset, size);
+	} else
+		size = 0;
+
+	return size;
+}
+
+static int hello_write(const char* path, const char *buf, size_t size, off_t offset,
+			struct fuse_file_info *fi)
+{
+	size_t len;
+	(void) fi;
+	if(strcmp(path, hello_path) != 0)
+		return -ENOENT;
+
+	len = strlen(hello_str);
+
+	if(offset + size != len)
+		return -ENOMEM;
+
+	memcpy(hello_str + offset, buf, size-1);
+	return size;
+}
+
+static int hello_truncate(const char *path, off_t size)
+{
+	return 0;
+}
+
+
+static struct fuse_operations hello_oper = {
+	.getattr	= hello_getattr,
+	.readdir	= hello_readdir,
+	.open		= hello_open,
+	.read		= hello_read,
+	.write		= hello_write,
+	.truncate	= hello_truncate,
+};
+
+int main(int argc, char *argv[])
+{
+	return fuse_main(argc, argv, &hello_oper, NULL);
+}
